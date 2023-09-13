be43dbb412c793fed965a441bbbeeca2fd9efdf1
cute <112333183+cute050@users.noreply.github.com>
2023-09-13 15:11:09 +0800

    Update meta.ts

--- docs/.vitepress/meta.ts
+++ docs/.vitepress/meta.ts
@@ -1,4 +1,4 @@
-export const name = '程序员小唐三'
+export const name = '程序员cute050'
 export const logo = 'https://chodocs.cn/chodocs-logo.svg'
 export const description = '一站式前端内容网站，包括学习路线、知识体系，海量前端面试问题解答，一站式阅读体验，跟随前沿技术，深度和广度学习，React 与 Vue 生态。'
 
