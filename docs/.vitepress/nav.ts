90f000266b2c3d9c36f9fb0662f651a47aa8a744
cute050 <yyyppp0502@outlook.com>
2023-09-19 16:34:29 +0800

    feature: 更新笔记地址

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,4 +1,6 @@
 export const nav = [
     {text: '首页', link: '/'},
     {text: '开始阅读', link: '/components/index.md'},
+    {text: '神领物流', link: '/components/study/index.md'},
+    {text: '在线判题项目', link: '/components/OJ/index.md'},
 ]
